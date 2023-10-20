dc429aedc191477c77091f5e22af507e73ed5868
Jack <jk19011999@gmail.com>
2023-10-20 22:58:03 +0530

    Created basic structure for the service

---
+++ src/main/java/com/esportarena/microservices/orchestrateapi/OrchestrateApplication.java
@@ -0,0 +1,13 @@
+package com.esportarena.microservices.orchestrateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrateApplication.class, args);
+	}
+
+}
